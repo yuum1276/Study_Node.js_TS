b3b846f64e013ace38a68e47187a8e2ad8fd57bf
yuum <dyalsla0105@gmail.com>
2023-04-10 23:20:49 +0900

    crud

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import express, { Request, Response, NextFunction } from 'express';
-import mysql, {FieldPacket, RowDataPacket} from 'mysql2/promise'
-import { IPost } from 'posts/Post';
+import mysql, { FieldPacket, RowDataPacket } from 'mysql2/promise';
+import Post, { IPost } from 'posts/Post';
 import IUser from 'users/User';
 
 const app = express();
@@ -11,7 +11,7 @@ const poolConfig = {
   host: 'localhost',
   user: 'root',
   password: 'dbal3326@@',
-  database: 'node_post'
+  database: 'node_post',
 };
 
 interface Token {
@@ -19,9 +19,9 @@ interface Token {
   token: string;
 }
 
-let token:Token = {
-  email: "",
-  token: ""
+let token: Token = {
+  email: '',
+  token: '',
 };
 
 let pool = mysql.createPool(poolConfig);
@@ -33,23 +33,22 @@ app.use((err: any, req: Request, res: Response, next: NextFunction) => {
 });
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }))
+app.use(express.urlencoded({ extended: false }));
 
 // User routes
 app.get('/users', async (req: Request, res: Response, next: NextFunction) => {
-  try{
-    const rows = await pool.query(`SELECT * FROM users`)
+  try {
+    const rows = await pool.query(`SELECT * FROM users`);
     console.log(rows);
-    res.send(rows)
-  }
-  catch(err){
+    res.send(rows);
+  } catch (err) {
     console.log(err);
-    next(err)
+    next(err);
   }
-    
 });
 
-app.get('/users/:id',
+app.get(
+  '/users/:id',
   async (req: Request, res: Response, next: NextFunction) => {
     const { id } = req.params;
     const connection = await pool.getConnection();
@@ -59,134 +58,234 @@ app.get('/users/:id',
         [id]
       );
       console.log(rows);
-      
+
       if (rows === null) {
         res.send('아이디가 없어용');
       }
-      res.send(rows)
+      res.send(rows);
     } catch (err) {
       next(err);
     }
   }
 );
 
-app.post('/join',
-  async(req: Request, res: Response, next: NextFunction) => {
-    const data = <IUser>req.body;
-    const connection = await pool.getConnection();
-    
-    try{
-      const [rows]:[IUser[], FieldPacket[]] = await connection.query('SELECT * FROM `users` WHERE `email` = ?', [data.email])
-      if(rows === null){
-        res.send({message:'사용중인 이멜이에용'})
-      } 
-      const result = await connection.query(
-        'INSERT INTO `users` (`email`, `nick`,`password`) VALUES (?, ?, ?)',
-        [data.email, data.nick, data.password]
-      )
-      res.send({
-        message: `${data.nick} 회원가입 성공! `
-      })
-    } catch(err){
-      console.log(err);
-      return(err)
+app.post('/join', async (req: Request, res: Response, next: NextFunction) => {
+  const data = <IUser>req.body;
+  const connection = await pool.getConnection();
+
+  try {
+    const [rows]: [IUser[], FieldPacket[]] = await connection.query(
+      'SELECT * FROM `users` WHERE `email` = ?',
+      [data.email]
+    );
+    if (rows === null) {
+      res.send({ message: '사용중인 이멜이에용' });
     }
+    const result = await connection.query(
+      'INSERT INTO `users` (`email`, `nick`,`password`) VALUES (?, ?, ?)',
+      [data.email, data.nick, data.password]
+    );
+    res.send({
+      message: `${data.nick} 회원가입 성공! `,
+    });
+  } catch (err) {
+    console.log(err);
+    return err;
   }
-)
+});
 
-app.post('/login',
-  async(req: Request, res: Response, next: NextFunction) => {
-    const data = <IUser>req.body;
+app.post('/login', async (req: Request, res: Response, next: NextFunction) => {
+  const data = <IUser>req.body;
 
-    const connection = await pool.getConnection();
+  const connection = await pool.getConnection();
 
-    const [rows]:[IUser[], FieldPacket[]] = await connection.query('SELECT * FROM `users` WHERE `email` = ?', [data.email]);
-    console.log(rows);
+  const [rows]: [IUser[], FieldPacket[]] = await connection.query(
+    'SELECT * FROM `users` WHERE `email` = ?',
+    [data.email]
+  );
+  console.log(rows);
 
-    if(rows.length > 0){
-      token.email = data.email;
-      token.token = 'asdf'
-      res.send({
-        message: '로그인 성공!',
-        token: token.token
-      })
-    } else {
-      res.send({
-        message: '로그인 실패'
-      })
-    }
-    await next();
+  if (rows.length > 0) {
+    token.email = data.email;
+    token.token = 'asdf';
+    res.send({
+      message: '로그인 성공!',
+      token: token.token,
+    });
+  } else {
+    res.send({
+      message: '로그인 실패',
+    });
   }
-)
+  await next();
+});
 
 // Post routes
 app.get('/posts', async (req: Request, res: Response, next: NextFunction) => {
   const connection = await pool.getConnection();
 
   try {
-    const rows = await connection.query(
-      'SELECT * FROM posts'
-    );
+    const rows = await connection.query('SELECT * FROM posts');
     res.send(rows);
   } catch (err) {
     next(err);
   }
 });
 
-app.post('/uploadPost', async (req: Request, res: Response, next:NextFunction) => {
-  const data = <IPost>req.body;
-  console.log(data);
-  try{
-
-  if(data.token === ""){
-    res.send({
-      message: "로그인 후 사용가능!"
-    })
-  }
-  const connection = await pool.getConnection();
-  const [rows]:[IUser[], FieldPacket[]] = await connection.query('SELECT * FROM `users` WHERE `email` = ?', [data.email]);
-  console.log(rows);
-  
-  if(rows !== null){
-    
-    if(token.email === data.email){
-
-      if(token.token === data.token){
-
-        const [result]:[IPost[], FieldPacket[]] = await connection.query(
-          'INSERT INTO `posts` (`title`, `content`,`email`) VALUES (?, ?, ?)',
-          [data.title, data.content, data.email]
-        )
-
-        console.log(result[0]);
-        return res.send(result[0])
-      } else {
-        return res.send({
-          message: '로그인 후 이용해주세용'
-        })
+app.post(
+  '/createPost',
+  async (req: Request, res: Response, next: NextFunction) => {
+    const data = <IPost>req.body;
+    console.log(data);
+    try {
+      if (data.token === '') {
+        res.send({
+          message: '로그인 후 사용가능!',
+        });
       }
-    } else { 
-      return res.send({
-        message: '로그인 후 이용해주세용'
-      })
+      const connection = await pool.getConnection();
+      const [rows]: [IUser[], FieldPacket[]] = await connection.query(
+        'SELECT * FROM `users` WHERE `email` = ?',
+        [data.email]
+      );
+      console.log(rows);
+
+      if (rows !== null) {
+        if (token.email === data.email) {
+          if (token.token === data.token) {
+            const [result]: [IPost[], FieldPacket[]] = await connection.query(
+              'INSERT INTO `posts` (`title`, `content`,`email`) VALUES (?, ?, ?)',
+              [data.title, data.content, data.email]
+            );
+
+            console.log(result[0]);
+            return res.send(result[0]);
+          } else {
+            return res.send({
+              message: '로그인 후 이용해주세용',
+            });
+          }
+        } else {
+          return res.send({
+            message: '로그인 후 이용해주세용',
+          });
+        }
+      }
+    } catch (err) {
+      next(err);
     }
   }
+);
 
-  } catch (err) {
-    next(err);
+app.put(
+  '/posts/:id',
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { id } = req.params;
+    const data = <IPost>req.body;
+    console.log(data);
+    try {
+      if (data.token === '') {
+        res.send({
+          message: '로그인 후 사용가능!',
+        });
+      }
+      const connection = await pool.getConnection();
+      const [rows]: [IUser[], FieldPacket[]] = await connection.query(
+        'SELECT * FROM `users` WHERE `email` = ?',
+        [data.email]
+      );
+      console.log(rows);
+
+      if (rows !== null) {
+        if (token.email === data.email) {
+          if (token.token === data.token) {
+            if (!data.title || !data.content) {
+              return res.send('제목, 내용은 필수!');
+            }
+
+            const [result] = await connection.query(
+              'UPDATE posts SET title = ?, content = ? WHERE id = ?',
+              [data.title, data.content, id]
+            );
+
+            if (result === null) {
+              return res.status(404).send('작성된 글이 없음!');
+            }
+
+            res.send('수정 완료!');
+          } else {
+            return res.send({
+              message: '로그인 후 사용가능!',
+            });
+          }
+        } else {
+          return res.send({
+            message: '로그인 후 사용가능!',
+          });
+        }
+      }
+    } catch (err) {
+      next(err);
+    }
   }
-})
+);
+
+app.delete(
+  '/posts/:id',
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { id } = req.params;
+    const data = <IPost>req.body;
+    console.log(data);
+    try {
+      if (data.token === '') {
+        res.send({
+          message: '로그인 후 사용가능!',
+        });
+      }
+      const connection = await pool.getConnection();
+      const [rows]: [IUser[], FieldPacket[]] = await connection.query(
+        'SELECT * FROM `users` WHERE `email` = ?',
+        [data.email]
+      );
+      console.log(rows);
+
+      if (rows !== null) {
+        if (token.email === data.email) {
+          if (token.token === data.token) {
+            const [result] = await connection.query(
+              'DELETE FROM posts WHERE id = ?',
+              [id]
+            );
+            if (result === null) {
+              return res.send('작성된 글이 없음!');
+            }
+            res.send('삭제 성공!');
+          } else {
+            return res.send({
+              message: '로그인 후 사용가능!',
+            });
+          }
+        } else {
+          return res.send({
+            message: '로그인 후 사용가능!',
+          });
+        }
+      }
+    } catch (err) {
+      next(err);
+    }
+  }
+);
 
 app.get(
   '/posts/:id',
   async (req: Request, res: Response, next: NextFunction) => {
-
     const { id } = req.params;
 
     const connection = await pool.getConnection();
 
     try {
-      const [rows]= await connection.query(
+      const [rows] = await connection.query(
         'SELECT id, title, content, created_at, updated_at FROM posts WHERE id = ?',
         [id]
       );
@@ -198,7 +297,7 @@ app.get(
       next(err);
     }
   }
-)
+);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
--- src/helper/db.ts
+++ src/helper/db.ts
@@ -2,17 +2,20 @@ import * as mysql from 'mysql2/promise';
 
 // Connections
 export async function dbConnection() {
-    let connection = await mysql.createConnection({
-        host: 'localhost',
-        user: 'root',
-        password: 'dbal3326@@'
-    });
+  let connection = await mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'dbal3326@@',
+  });
 
-    connection.connect()
-        .then(() => connection.query<mysql.RowDataPacket[]>('SELECT 1 + 1 AS solution'))
-        .then(([rows, fields]) => {
-            console.log('The solution is: ', rows[0]['solution']);
-        });
+  connection
+    .connect()
+    .then(() =>
+      connection.query<mysql.RowDataPacket[]>('SELECT 1 + 1 AS solution')
+    )
+    .then(([rows, fields]) => {
+      console.log('The solution is: ', rows[0]['solution']);
+    });
 }
 
 dbConnection();
